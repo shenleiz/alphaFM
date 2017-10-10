748644924baabbf53531895987956d78c6bf60e4
shenlei.zhong <shenlei.zhong@mobvista.com>
2017-10-10 11:24:11 +0800

    捕捉数据异常

--- src/Sample/fm_sample.h
+++ src/Sample/fm_sample.h
@@ -3,7 +3,7 @@
 
 #include <string>
 #include <vector>
-
+#include <exception>
 using namespace std;
 
 const string spliter = " ";
@@ -21,40 +21,47 @@ public:
 
 fm_sample::fm_sample(const string& line) 
 {
-    this->x.clear();
-    size_t posb = line.find_first_not_of(spliter, 0);
-    size_t pose = line.find_first_of(spliter, posb);
-    int label = atoi(line.substr(posb, pose-posb).c_str());
-    this->y = label > 0 ? 1 : -1;
-    string key;
-    double value;
-    while(pose < line.size())
-    {
-        posb = line.find_first_not_of(spliter, pose);
-        if(posb == string::npos)
-        {
-            break;
-        }
-        pose = line.find_first_of(innerSpliter, posb);
-        if(pose == string::npos)
-        {
-            cout << "wrong line input\n" << line << endl;
-            throw "wrong line input";
-        }
-        key = line.substr(posb, pose-posb);
-        posb = pose + 1;
-        if(posb >= line.size())
-        {
-            cout << "wrong line input\n" << line << endl;
-            throw "wrong line input";
-        }
-        pose = line.find_first_of(spliter, posb);
-        value = stod(line.substr(posb, pose-posb));
-        if(value != 0)
-        {
-            this->x.push_back(make_pair(key, value));
-        }
-    }
+	try
+	{
+		this->x.clear();
+		size_t posb = line.find_first_not_of(spliter, 0);
+		size_t pose = line.find_first_of(spliter, posb);
+		int label = atoi(line.substr(posb, pose-posb).c_str());
+		this->y = label > 0 ? 1 : -1;
+		string key;
+		double value;
+		while(pose < line.size())
+		{
+			posb = line.find_first_not_of(spliter, pose);
+			if(posb == string::npos)
+			{
+				break;
+			}
+			pose = line.find_first_of(innerSpliter, posb);
+			if(pose == string::npos)
+			{
+				cout << "wrong line input\n" << line << endl;
+				throw "wrong line input";
+			}
+			key = line.substr(posb, pose-posb);
+			posb = pose + 1;
+			if(posb >= line.size())
+			{
+				cout << "wrong line input\n" << line << endl;
+				throw "wrong line input";
+			}
+			pose = line.find_first_of(spliter, posb);
+			value = stod(line.substr(posb, pose-posb));
+			if(value != 0)
+			{
+				this->x.push_back(make_pair(key, value));
+			}
+		}
+	}
+	catch (exception& e)
+	{
+		cout <<"wrong line input: " << line << " ----" <<e.what() << endl;
+	}
 }
 
 
